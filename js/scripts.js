9d5cb9949c38a3a484bb6c0f782d9af5346fffad
alcibiades13 <branislav.teofilovic@gmail.com>
2015-12-08 02:16:54 +0100

    responsive image slider added

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,8 @@ $(document).ready(function () {
 		progressBar();
 		prettyPrint();
 		copyMarkup();
+		slider();
+		expandSlider();
 	}
 
 
@@ -53,6 +55,126 @@ $(document).ready(function () {
 	$('.anim-progress').on('click', animProgress);
 
 
+	// slider
+	function slider() {
+		$('.slider-next').on('click', function (e) {
+			e.preventDefault();
+
+			// slide container	
+			$slider = $(this).parent();
+			var numOfSlides = $slider.find('.slide').length;
+
+			// index of active and next slide
+			var curIndex = $slider.find('.active').index(),
+				nextIndex;
+
+			$slider.find('.active').removeClass('active');
+
+			// set the next active slide
+			if(curIndex >= numOfSlides - 1) {
+				nextIndex = 0;
+			} 
+			else {
+				nextIndex = curIndex + 1;
+			}
+
+			$slider.find('.slide').eq(nextIndex).css(
+				{
+					'visibility': 'visible',
+					'opacity': 1
+				}).siblings().css(
+				{
+					'visibility': 'hidden',
+					'opacity': 0
+				}
+			);
+			$slider.find('.slide').eq(nextIndex).addClass('active');
+
+		});
+
+		$('.slider-prev').on('click', function (e) {
+			e.preventDefault();
+
+			// slide container	
+			$slider = $(this).parent();
+			var numOfSlides = $slider.find('.slide').length;
+
+			// index of active and prev slide
+			var curIndex = $slider.find('.active').index(),
+				prevIndex;
+
+			$slider.find('.active').removeClass('active');
+
+			// get image height to expand the slider height
+			var imgHeight = $slider.find('.slide img').height();
+			console.log(imgHeight);
+
+			$slider.css({
+				'min-height': imgHeight
+			});
+
+			// set the prev active slide
+			if(curIndex === 0) {
+				prevIndex = numOfSlides - 1;
+			} 
+			else {
+				prevIndex = curIndex - 1;
+			}
+
+			$slider.find('.slide').eq(prevIndex).css(
+				{
+					'visibility': 'visible',
+					'opacity': 1
+				}).siblings().css(
+				{
+					'visibility': 'hidden',
+					'opacity': 0
+				}
+			);
+			$slider.find('.slide').eq(prevIndex).addClass('active');
+
+		});
+
+		/* autoplay slider
+		* clear interval on mouseenter
+		* reset on mouseleave
+		*/
+		if($('.slider-next').parent().hasClass('slider-play')) {
+
+			var sliderInt;
+
+			sliderInt = setInterval( sliderInterval, 5000);
+
+			$('.slider-play').on({
+			    mouseenter: function() {
+			        clearInterval( sliderInt );
+			    },
+			    mouseleave: function() {
+			        sliderInt = setInterval( sliderInterval, 5000 );
+			    }
+			});
+
+			function sliderInterval(){
+				$('.slider-play .slider-next').trigger('click');
+			}
+		}
+
+
+
+	}
+
+	function expandSlider() {
+
+		// get image height to expand the slider height
+		var imgHeight = $('.slider').find('.slide img').height();
+		$('.slider').css({
+			'min-height': imgHeight
+		}).find('.slide').css('min-height', imgHeight);
+
+	}
+
+
+
 	// theme functions
 
 	// copy markup
@@ -68,5 +190,7 @@ $(document).ready(function () {
 	}
 
 
+	// expand slider minimum height on resize, otherwise it will be zero
+	$(window).resize(expandSlider);
 	init();
 });
